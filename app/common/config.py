ae87eb409ab45c19e8f0c6fc493f0728836e82e9
JetHerun <869572033@qq.com>
2023-10-22 23:05:03 +0800

    Update config.py

--- app/common/config.py
+++ app/common/config.py
@@ -92,7 +92,7 @@ class Config(QConfig):
 
 YEAR = 2023
 AUTHOR = "Zaphkiel"
-VERSION = "0.8.1"
+VERSION = "0.8.2-beta"
 FEEDBACK_URL = "https://github.com/Zzaphkiel/Seraphine/issues"
 GITHUB_URL = "https://github.com/Zzaphkiel/Seraphine"
 
